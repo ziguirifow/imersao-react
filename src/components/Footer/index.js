3ec24acade59cafb753b558b97e2aca27121a2e0
Thulio <thulio.alves@gmail.com>
2020-07-31 22:47:59 -0300

    Footer com scroll

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,24 +5,22 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <Link to="https://github.com/ziguirifow">
-        <img src="https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg" alt="Logo Github" />
-      </Link>
-      <Link to="https://www.linkedin.com/in/thulio-alves-gomes/">
-        <img src="https://oficinadainteligencia.com.br/wp-content/uploads/2019/04/simple-icon-linkedin-white.png" alt="Logo Linkedin" />
-      </Link>
 
-      {/* <Link to="/">
-        <img className="Logo" src={Logo} alt="TuioFlix logo" />
-      </Link> */}
+      <a href="https://github.com/ziguirifow" rel="noopener noreferrer" target="_blank">
+        <img src="https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg" alt="Logo Github" />
+      </a>
+      <a href="https://www.linkedin.com/in/thulio-alves-gomes/" rel="noopener noreferrer" target="_blank">
+        <img src="https://oficinadainteligencia.com.br/wp-content/uploads/2019/04/simple-icon-linkedin-white.png" alt="Logo Linkedin" />
+      </a>
 
       <p>
         Página desenvolvida por
         {' '}
-        <Link to="/">Thulio</Link>
+        <Link to="/" onclick={window.scroll({ top: 0, left: 0, behavior: 'smooth' })}>Thulio</Link>
         {' '}
         durante a #ImersãoReact da Alura
       </p>
+
     </FooterBase>
   );
 }
